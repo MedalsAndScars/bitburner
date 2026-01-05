e71433ab079991af5b46462a4ac956f1300f0a26
manfred.wernhardt@gmail.com <manfred.wernhardt@gmail.com>
2026-01-05 01:23:32 +0100

    added http worm to open ports function

--- hackscripts/prep_host.js
+++ hackscripts/prep_host.js
@@ -1,19 +1,17 @@
 /** @param {NS} ns */
 export async function main(ns, firstnode = ns.args[0]) {
-  while (true) {
-    let hosts = ns.scan(firstnode);
-    for (let i = 0; i <= hosts.length - 1; i++) {
-      let subhost = ns.scan(hosts[i]);
-      for (let j = 0; j <= subhost.length - 1; j++) {
-        if (subhost[j] != firstnode) {
-          openports(ns, subhost[j]);
-        }
+  let hosts = ns.scan(firstnode);
+  for (let i = 0; i <= hosts.length - 1; i++) {
+    let subhost = ns.scan(hosts[i]);
+    for (let j = 0; j <= subhost.length - 1; j++) {
+      if (subhost[j] != firstnode) {
+        openports(ns, subhost[j])
       }
-      openports(ns, hosts[i]);
     }
-    await ns.sleep(3000);
+    openports(ns, hosts[i])
   }
 }
+
 
 async function openports(ns, host = ns.args[0]) {
   if (ns.getServerRequiredHackingLevel(host) <= ns.getHackingLevel() && !ns.hasRootAccess(host)) {
@@ -24,8 +22,14 @@ async function openports(ns, host = ns.args[0]) {
       ns.ftpcrack(host);
     }
     if (ns.fileExists("relaySMTP.exe")) {
-      ns.relaysmtp(host);
+      ns.relaysmtp(host)
+    }
+    if (ns.fileExists("HTTPWorm.exe")){
+      ns.httpworm(host)
     }
     ns.nuke(host);
+  }
+  else{
+    stop
   }
 }
